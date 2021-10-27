c89f0252626bb9af6659ec8fb617372588a33d2f
Habibov Ulug'bek <habibovulugbek22@gmail.com>
2021-10-27 10:00:22 +0500

    added style and navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {Switch , Route , Link} from "react-router-dom";
 import { Layout , Typography, Space } from "antd"
 
-import {Navbar } from "./components"
+import {Navbar } from "./components";
+import "./App.css"
 
 const App = () => {
     return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Button , Typography, Menu , Avatar} from "antd"
 import {Link} from "react-router-dom"
+import { HomeOutlined , MoneyCollectOutlined ,  BulbOutlined, FundOutlined, MenuOutlined} from "@ant-design/icons"
 
 import icon from "../images/cryptocurrency.png"
+import { MenuItem } from 'rc-menu';
 
 const Navbar = () => {
     return (
@@ -12,9 +14,26 @@ const Navbar = () => {
                 <Typography.Title level={2} className="logo">
                     <Link to="/">Cryptoverse</Link>
                 </Typography.Title>
-                <Button className="menu-control-container">
-
-                </Button>
+                <Menu theme="dark">
+                    <MenuItem icon={<HomeOutlined />} >
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </MenuItem>
+                    <MenuItem icon={<FundOutlined />} >
+                        <Link to="/cryptocurrencies">
+                            Cryptocurrencies
+                        </Link>
+                    </MenuItem><MenuItem icon={<MoneyCollectOutlined />} >
+                        <Link to="/exchanges">
+                            Exchanges
+                        </Link>
+                    </MenuItem><MenuItem icon={<BulbOutlined />} >
+                        <Link to="/news">
+                            News
+                        </Link>
+                    </MenuItem>
+                </Menu>
             </div>
         </div>
     )
