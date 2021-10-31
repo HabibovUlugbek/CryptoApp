916167b98531d6d46c3173e00719883704b21a5a
Habibov Ulug'bek <habibovulugbek22@gmail.com>
2021-10-31 23:35:35 +0500

    added cryptodetails

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -36,7 +36,7 @@ const Cryptocurrencies = ({simplified}) => {
                         <Link to={`/crypto/${currency.id}`}>
                             <Card 
                                 title={`${currency.rank}. ${currency.name}`}
-                                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                                extra={<img className="crypto-image" src={currency.iconUrl} alt="crypto-img"/>}
                                 hoverable
                             >
                                 <p>Price: {millify(currency.price)}</p>
